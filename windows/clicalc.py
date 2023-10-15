5a40f89e62d7ecfdbfe8289e89afcc2ce3140b6b
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-10-15 12:23:15 +0200

    minor improvements

--- windows/clicalc.py
+++ windows/clicalc.py
@@ -18,34 +18,31 @@ def work(arg,mode):
     for x in letras: arg=arg.replace(x,str(letras[x]))
     arg=arg.replace("Ans",str(buffer))
     arg=arg.replace('"',"'")
-    if ("input(" in arg or "print(" in arg): print(error) 
-    else:
-        try: result=str(eval(arg))
-        except: result=""
-        if len(result)==0: print(error)
-        elif mode==1:
+    cond1=("exit()" in arg or "quit()" in arg)
+    cond2=("print" in arg or "input" in arg)
+    if not (cond1 or cond2):
+        try: result=eval(arg)
+        except: result=None
+        if mode==1:
             print(resultprt+str(result))
             buffer=result
         elif mode==2:
             print(scadd+str(result))
-            try: float(result)
-            except: result='"'+result+'"'
             return result
+    else: print(error)
     
 def main(arg1=False):
     global letras, buffer
-    init()
-    buffer=0; fix=False
+    init(); buffer=0; fix=False
     while True:
         if arg1==False:
             print(operaprt,end="")
             arg=input()
         else: arg=arg1
-        if fix==True:
-            work("",3)
-            fix=False
+        if fix==True: work("",3); fix=False
         if arg=="exit": print(""); break
         elif arg=="clear": cmd("CLS")
+        elif arg=="flush": letras={":a":0,":b":0,":c":0,":d":0}
         else:
             if "set" in arg:
                 letra=arg[arg.find("set(")+4:arg.find(")")]
