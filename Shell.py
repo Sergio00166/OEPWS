e96ec3995c2ec8e0f2b7e1bca0534cb378e47ffa
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-10-18 09:21:16 +0200

    Update Shell.py

--- Shell.py
+++ Shell.py
@@ -5,7 +5,7 @@ from os import environ, getcwd
 from sys import setrecursionlimit, path, argv
 from subprocess import check_output
 
-version="alfa v0.1.60.0"; line=" -"
+version="alfa v0.1.60.1"; line=" -"
 
 def clsmenu(): cmd("CLS"); print(menu,end="\n\n")
 
