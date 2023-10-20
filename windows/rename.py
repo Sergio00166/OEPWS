4962dbdfa0e99e7185e725ef6481082b81578288
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-10-20 12:32:41 +0200

    fix error codes

--- windows/rename.py
+++ windows/rename.py
@@ -18,30 +18,32 @@ def main(arg1,directory):
             fich=fich.replace("\f","to").replace("\n","from")
             fich=fich.split("::")
             direct=arg1[fro+6:to]
-            direct=fixaddr(direct)
-            dest=dest.replace("\f","to").replace("\n","from")
-            dest=dest.split("::")
-            for x in range(0,len(fich)):
-                if not ":\\" in fich[x]:
-                    if chr(92) in fich[x]:
-                         fix=fich[x].split(chr(92))
-                         path=fix; path.pop(); buff=""
-                         for i in path: buff+=i+chr(92)
-                         fl=direct+chr(92)+fich[x]
-                         dt=direct+chr(92)+buff+dest[x]
-                         
+            direct=fixaddr(direct, True)
+            if not direct==None:
+                dest=dest.replace("\f","to").replace("\n","from")
+                dest=dest.split("::")
+                for x in range(0,len(fich)):
+                    if not ":\\" in fich[x]:
+                        if chr(92) in fich[x]:
+                             fix=fich[x].split(chr(92))
+                             path=fix; path.pop(); buff=""
+                             for i in path: buff+=i+chr(92)
+                             fl=direct+chr(92)+fich[x]
+                             dt=direct+chr(92)+buff+dest[x]
+                             
+                        else:
+                            fl=direct+chr(92)+fich[x]
+                            dt=direct+chr(92)+dest[x]
                     else:
-                        fl=direct+chr(92)+fich[x]
-                        dt=direct+chr(92)+dest[x]
-                else:
-                    fl=fich[x]
-                    tmp=fl.split(chr(92))
-                    tmp.pop(); buff=""
-                    for i in tmp: buff=buff+i+chr(92)
-                    buff=fixaddr(buff)
-                exp='move "'+fl+'" "'+dt+'"'
-                exp=str(exp).replace(chr(92)+chr(92),chr(92))
-                cmd(exp+" 2>nul", shell=True)
+                        fl=fich[x]
+                        tmp=fl.split(chr(92))
+                        tmp.pop(); buff=""
+                        for i in tmp: buff=buff+i+chr(92)
+                        buff=fixaddr(buff, True)
+                    exp='move "'+fl+'" "'+dt+'"'
+                    exp=str(exp).replace(chr(92)+chr(92),chr(92))
+                    cmd(exp+" 2>nul", shell=True)
+            else: print(color("\n   Error\n", "R"))
         else:
             fich=arg1[:to]
             fich=fich.replace("\f","to").replace("\n","from")
@@ -53,9 +55,13 @@ def main(arg1,directory):
                 if ":"+chr(92) in fich[x]:
                     dirt=fich[x].split(chr(92));file=dirt.pop(); direct=""
                     for i in dirt: direct+=i+chr(92)
-                    direct=fixaddr(direct)
-                    exp='move "'+direct+file+'" "'+direct+dest[x]+'"'
+                    direct=fixaddr(direct, True)
+                    if not direct==None:
+                        exp='move "'+direct+file+'" "'+direct+dest[x]+'"'
                 else: exp='move "'+directory+fich[x]+'" "'+directory+dest[x]+'"'
-                exp=str(exp).replace(chr(92)+chr(92),chr(92))
-                cmd(exp+" 2>nul", shell=True)
+                if not direct==None:
+                    exp=str(exp).replace(chr(92)+chr(92),chr(92))
+                    cmd(exp+" 2>nul", shell=True)
+                else: print(color("\n   Error\n", "R"))
     except: print(color("\n   Error\n", "R"))
+
