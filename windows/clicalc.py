90f032307c49161ea41e1ce8dceaacb96fbafa0b
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-10-27 14:52:03 +0200

    A lot fixes and improvements

--- windows/clicalc.py
+++ windows/clicalc.py
@@ -1,45 +1,53 @@
 #Code by Sergio1260
 
-from sys import path
-from math import *
 from os import system as cmd
+from sys import path
+import math
+math = {x:getattr(math, x) for x in dir(math)}
+from math import *
+
+def fix(code):
+    import ast
+    banned_functions = {'print', 'exit', 'quit', 'exec', 'eval', 'val', 'init', 'fix', 'work', 'main', 'cmd', 'color'}
+    try:
+        tree = ast.parse(code)
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Call) and (isinstance(node.func, ast.Name) and node.func.id in banned_functions): return False
+            if isinstance(node, ast.Import) or isinstance(node, ast.ImportFrom):
+                for name in node.names:
+                    module_name = name.name.split('.')[0]
+                    if module_name in banned_modules: return False
+        return True
+    except SyntaxError: return False
 
 def init():
+    global letras, operaprt, error, resultprt
     from colors import color
-    global letras, operaprt, error, resultprt, scadd
     letras={":a":0,":b":0,":c":0,":d":0}
     operaprt=(str(color("\n Operation: ","G-") + color()))
     error=str(color("\n   Error","R"))
     resultprt=str(color("\n  Result: ","B"))
-    scadd=str(color("\n  Succesfully added: ","B"))
-    
+   
 def work(arg,mode):
-    global temp, letras, operaprt, error, resultprt, scadd, buffer
+    global letras, operaprt, error, resultprt, buffer
     for x in letras: arg=arg.replace(x,str(letras[x]))
     arg=arg.replace("Ans",str(buffer))
     arg=arg.replace('"',"'")
-    cond1=("exit()" in arg or "quit()" in arg)
-    cond2=("print" in arg or "input" in arg)
-    if not (cond1 or cond2):
-        try: result=eval(arg)
+    if fix(arg):
+        try: result=eval(arg,{},math)
         except: result=None
-        if mode==1:
-            print(resultprt+str(result))
-            buffer=result
-        elif mode==2:
-            print(scadd+str(result))
-            return result
-    else: print(error)
+    else: result=None
+    if mode==1: print(resultprt+str(result)); buffer=result
+    elif mode==2: return result
     
 def main(arg1=False):
     global letras, buffer
-    init(); buffer=0; fix=False
+    init(); buffer=0
     while True:
         if arg1==False:
             print(operaprt,end="")
             arg=input()
         else: arg=arg1
-        if fix==True: work("",3); fix=False
         if arg=="exit": print(""); break
         elif arg=="clear": cmd("CLS")
         elif arg=="flush": letras={":a":0,":b":0,":c":0,":d":0}
@@ -48,8 +56,7 @@ def main(arg1=False):
                 letra=arg[arg.find("set(")+4:arg.find(")")]
                 for x in letras:
                     if x.replace(":","")==letra:
-                        letras[x]=work(arg.replace("set("+letra+")",""),2)
-                        fix=True        
+                        letras[x]=work(arg.replace("set("+letra+") ",""),2)
             else: work(arg,1)
 
         if arg1!=False: print(""); break
