bb0b4279365ebbf3d365326ad7d4a5253519cb5b
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-11-21 09:38:22 +0100

    Add files via upload

--- import/colors.py
+++ import/colors.py
@@ -1,23 +1,27 @@
 #Styles.css in pyhton :)
 
 def color(arg="",color=""):
-    if color=="B": return "[34m[1m"+arg+"[0m"
-    elif color=="Bnr": return "[34m[1m"+arg
-    elif color=="G": return "[32m[1m"+arg+"[0m"
-    elif color=="Gnr": return "[32m[1m"+arg
-    elif color=="G-": return "[32m[22m"+arg+"[0m"
-    elif color=="R": return "[31m[1m"+arg+"[0m"
-    elif color=="Rnr": return "[31m[1m"+arg
-    elif color=="W": return "[37m[1m"+arg+"[0m"
-    elif color=="Ynr": return "[33m[1m"+arg
-    elif color=="Y": return "[33m[1m"+arg+"[0m"
-    elif color=="Y-": return "[33m[2m"+arg+"[0m"
-    elif color=="M": return "[35m[22m"+arg+"[0m"
-    elif color=="Mnr": return "[35m[22m"+arg
-    elif color=="C": return "[36m[22m"+arg+"[0m"
-    elif color=="B-": return "[34m[22m"+arg
-    elif color=="bW": return "[46m[1m"+arg+"[0m"
-    elif color=="By": return "[34m[1m"+arg+"[33m[1m[0m"
-    elif color=="W": return "[36m[1m"+arg+"[0m"
-    elif color=="nrY": return "[33m[1m"+arg
-    else: return "[0m"
+    from sys import path
+    path.append('import/')
+    from colorama import init, Fore, Back, Style
+    init(autoreset = False)
+    if color=="B": return Fore.BLUE + Style.BRIGHT + arg + Style.RESET_ALL
+    elif color=="Bnr": return Fore.BLUE + Style.BRIGHT + arg
+    elif color=="G": return Fore.GREEN + Style.BRIGHT + arg + Style.RESET_ALL
+    elif color=="Gnr": return Fore.GREEN + Style.BRIGHT + arg
+    elif color=="G-": return Fore.GREEN + Style.DIM + arg + Style.RESET_ALL
+    elif color=="R": return Fore.RED + Style.BRIGHT + arg + Style.RESET_ALL
+    elif color=="Rnr": return Fore.RED + Style.BRIGHT + arg
+    elif color=="W": return Fore.WHITE + Style.BRIGHT + arg + Style.RESET_ALL
+    elif color=="Ynr": return Fore.YELLOW + Style.BRIGHT + arg
+    elif color=="Y": return Fore.YELLOW + Style.BRIGHT + arg + Style.RESET_ALL
+    elif color=="Y-": return Fore.YELLOW + Style.DIM + arg + Style.RESET_ALL
+    elif color=="M": return Fore.MAGENTA + Style.NORMAL + arg + Style.RESET_ALL
+    elif color=="Mnr": return Fore.MAGENTA + Style.NORMAL + arg
+    elif color=="C": return Fore.CYAN + Style.NORMAL + arg + Style.RESET_ALL
+    elif color=="B-": return Fore.BLUE + Style.NORMAL + arg
+    elif color=="bW": return Back.CYAN + Style.BRIGHT + arg + Style.RESET_ALL
+    elif color=="By": return Fore.BLUE + Style.BRIGHT + arg + Fore.YELLOW + Style.BRIGHT + Style.RESET_ALL
+    elif color=="W": return Fore.CYAN + Style.BRIGHT + arg + Style.RESET_ALL
+    elif color=="nrY": return Fore.YELLOW + Style.BRIGHT + arg
+    else: return Style.RESET_ALL
