2994bd9befd3b4e9e7c6f1e1513e0f7d19814efa
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2023-12-18 08:24:04 +0100

    Update Shell.py

--- Shell.py
+++ Shell.py
@@ -1,4 +1,4 @@
-#Code by Sergio1260
+ #Code by Sergio1260
 
 from os import system as cmd
 from os import environ, getcwd
@@ -23,37 +23,39 @@ def var(arg1):
 
 def cli(args):
     global directory
-    if not len(oldir)==1:
-        if not directory==oldir[1]: oldir.append(directory)
-    else: oldir.append(directory)
-    if not args:
-        print(fix+directory+flechas, end="")
-        try: a=str(input())
-        except: a=""
-    else: a=args+" "
-    a=forvar(a, directory)
-    inp=a.replace("';'","\f").split(";")
-    for a in inp:
-        a=var(a+" ").replace("\f",";").replace("';'", ";")
-        a=a.lstrip().rstrip()
-        if " " in a:
-            arg=a[:a.find(" ")]
-            arg1=a[a.find(" ")+1:].lstrip().rstrip()
-        else: arg=a; arg1=""
-        if arg=="exit": 
-            database("mp3","-quit","","")
-            exit()
-        elif arg=="clear": clsmenu()
-        else:
-            if not arg=="":
-                if arg=="sudo": arg1=arg1.replace(";","';'")
-                try: directory=database(arg,arg1,directory,str(oldir[0]))
-                except KeyboardInterrupt: pass
-                except: print(ferror)
-                if arg=="flmgr": clsmenu()
-                if not len(oldir)==1:
-                    if not directory==oldir[1]: oldir.pop(0)
-    if args: input("Press any key to exit . . . ")
+    try:
+        if not len(oldir)==1:
+            if not directory==oldir[1]: oldir.append(directory)
+        else: oldir.append(directory)
+        if not args:
+            print(fix+directory+flechas, end="")
+            try: a=str(input())
+            except: a=""
+        else: a=args+" "
+        a=forvar(a, directory)
+        inp=a.replace("';'","\f").split(";")
+        for a in inp:
+            a=var(a+" ").replace("\f",";").replace("';'", ";")
+            a=a.lstrip().rstrip()
+            if " " in a:
+                arg=a[:a.find(" ")]
+                arg1=a[a.find(" ")+1:].lstrip().rstrip()
+            else: arg=a; arg1=""
+            if arg=="exit": 
+                database("mp3","-quit","","")
+                exit()
+            elif arg=="clear": clsmenu()
+            else:
+                if not arg=="":
+                    if arg=="sudo": arg1=arg1.replace(";","';'")
+                    try: directory=database(arg,arg1,directory,str(oldir[0]))
+                    except KeyboardInterrupt: pass
+                    except: print(ferror)
+                    if arg=="flmgr": clsmenu()
+                    if not len(oldir)==1:
+                        if not directory==oldir[1]: oldir.pop(0)
+        if args: input("Press any key to exit . . . ")
+    except KeyboardInterrupt: pass
     
 def main(args):
     global directory
@@ -67,35 +69,3 @@ def main(args):
         else: cli(args)
     else:
         while True: cli(False)
-        
-if __name__ == "__main__":
-    cmd('TITLE OEPWS shell')
-    setrecursionlimit(10**6)
-    for x in path:
-        if "site-packages" in x: path.remove(x)
-    for x in path:
-        if "site-packages" in x: path.remove(x)
-    path.append(path[0]+chr(92)+"import")
-    path.append(path[0]+chr(92)+"import\\other")
-    from colors import color
-    menu=str(color(" OEPWS by Sergio1260 ","G")+"\n "+
-             color(version,"R")+color(" on ","G")+
-             color("Windows OS","B"))
-    clsmenu(); from forvar import main as forvar
-    flechas=str(color(" >> ","G-"))
-    ferror=color("\n  FATAL ERROR\n","R")
-    raw=str(check_output("echo %userprofile%",shell=True))
-    userdir=raw[2:len(raw)-4].replace(chr(92)+chr(92),chr(92))
-    userdir=userdir.replace(chr(92),chr(92)+chr(92))
-    path.append(path[0] + chr(92) + 'windows')
-    from database import database
-    user=str(check_output("whoami")); oldir=[]
-    user=user[2:len(user)-5]; args=" ".join(argv[1:])
-    user=user[user.find(chr(92)+chr(92))+2:]
-    del raw; directory=getcwd()+chr(92)
-    directory=directory.replace("\\\\","\\")
-    oldir.append(directory)
-    fix="\r "+str(color(user,"B"))+str(color(" ","B-"))
-
-    main(args)
-    
