0550732c45b5444523e8ba2b4b3323a9aeac5c40
Sergio1260 <35770464+Sergio00166@users.noreply.github.com>
2024-03-01 11:04:32 +0100

    Minorm changes

--- import/other/syntax.py
+++ import/other/syntax.py
@@ -1,61 +1,53 @@
 # Code by Sergio1260
 
 from os.path import join, isabs
+from re import findall
+
+def parser(text, keywords):
+    try:
+        pattern = r'"([^"]+)"|(\b\w+\b)'
+        matches = findall(pattern, text)
+        keyword_indices = []
+        for keyword in keywords:
+            for i, match in enumerate(matches):
+                if match[1] == keyword:
+                    keyword_indices.append(i)
+                    break
+        if keyword_indices != sorted(keyword_indices): raise SyntaxError
+        parsed_groups = []; start_index = 0
+        for index in keyword_indices:
+            before_keyword = [match[0] for match in matches[start_index:index] if match[0]]
+            parsed_groups.append(before_keyword)
+            start_index = index + 1
+        after_last_keyword = [match[0] for match in matches[start_index:] if match[0]]
+        parsed_groups.append(after_last_keyword)
+
+        return parsed_groups
+    
+    except: raise SyntaxError
+
 
 def parse_syntax(code, directory, mode=["from", "to"]):
-
     # Example
     # If second mode is None
     # read "file" from "path"
     # If second mode is "to"
     # copy "file" from "path" to "destination"
 
-    from_str = '" '+mode[0]+' "'
-    dest_str = mode[1]
-    if not dest_str == None:
-        dest_str = '" '+dest_str+' "'
-
+    mode = [mode[0], mode[1]] if not mode[1]==None else [mode[0]]
     try:
-        is_from = from_str in code
-        if is_from:
-            code = code.split(from_str)
-            arg = code[0]
-            if not dest_str == None:
-                code = code[1].split(dest_str)
-                from_arg = code[0]
-                to = code[1]
-            else: from_arg = code[1][:-1]
-            if not isabs(from_arg):
-                from_arg = join(directory, from_arg)
-
-        else:
-            from_arg = directory
-            if not dest_str == None:
-                code = code.split(dest_str)
-                arg = code[0]
-                to = code[1]
-            else: arg = code
-
-        arg = arg.split('" "')
-        arg[0] = arg[0][1:]
-        out = []
-
-        if dest_str == None and not is_from:
-            arg[-1] = arg[-1][:-1]
-
-        for x in arg:
-            if isabs(x): out.append(x)
-            else: out.append(join(from_arg, x))
-
-        if not dest_str == None:
-            to = to.split('" "')
-            to[-1] = to[-1][:-1]
-            out1 = []
-            for x in to:
-                if isabs(x): out1.append(x)
-                else: out1.append(join(directory, x))
-            return out, out1
-
+        code = parser(code, mode)
+        arg = code[0]; lenght=len(code); lenm=len(mode)
+        if lenm>1:
+            from_arg=code[1][0] if lenght>2 else directory
+            if lenght>2: to = code[2]
+            elif lenght==1: arg=code[0]
+            else: to = code[1]
+        else: from_arg=code[1][0] if lenght>1 else directory
+        if not isabs(from_arg): from_arg = join(directory, from_arg)
+        if len(arg)==0: arg=[""]
+        out = [x if isabs(x) else join(from_arg, x) for x in arg]
+        if lenm>1: return out, [x if isabs(x) else join(directory, x) for x in to]
         else: return out
-
     except: raise SyntaxError
+
